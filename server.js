3cd438ae71fe03dfecd44c0083867bc8f66642d2
samsmith453 <Sam@Admin.local>
2017-04-18 20:45:44 +0100

    Port update

--- server.js
+++ server.js
@@ -11,4 +11,4 @@ app.post("/", upload.single('file'), function(req, res){
      res.send({"size": req.file.size});
 });
 
-var server = app.listen(process.argv.PORT);
+var server = app.listen(process.env.PORT);
