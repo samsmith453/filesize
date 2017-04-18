81f2a42f25e0e2b3ea22ecaa705509352869d94a
samsmith453 <Sam@Admin.local>
2017-04-18 20:43:16 +0100

    Change port

--- server.js
+++ server.js
@@ -11,4 +11,4 @@ app.post("/", upload.single('file'), function(req, res){
      res.send({"size": req.file.size});
 });
 
-var server = app.listen(3000);
+var server = app.listen(process.argv.PORT);
