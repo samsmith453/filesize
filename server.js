23fbc28efa0e9f6de0f73f604a625b230c15f39c
samsmith453 <Sam@Admin.local>
2017-04-18 20:28:02 +0100

    Initial Commit

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var app = express();
+var multer = require("multer");
+var upload = multer({dest: "uploads/"});
+
+app.get("/", function(req, res){
+     res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/", upload.single('file'), function(req, res){
+     res.send({"size": req.file.size});
+});
+
+var server = app.listen(3000);
